d56eb540599697ad35d3d3607e18de68572795aa
Kovács Lajos <kovacs.lajos1218@gmail.com>
2019-10-09 22:26:09 +0200

    Added Loader tests

--- test/Loader.test.js
+++ test/Loader.test.js
@@ -1,9 +1,11 @@
 const chai = require('chai')
+const Loader = require('../lib/Loader')
 
 const expect = chai.expect
 
 describe('Loader tests', () => {
-	it('should true', () => {
-		expect(true).to.be.true
+	it('should create an instance of Loader with deafult state', () => {
+		let loader = new Loader()
+		expect(loader._workerCount()).to.be.equal(0)
 	})
 })
